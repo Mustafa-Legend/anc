dbb289e69570556d71cadb81faae727d64a2e0b4
Mustafa Ismail <35161146+Mustafa-Legend@users.noreply.github.com>
2025-07-17 16:40:30 +0300

    Update [username].js

--- pages/[username].js
+++ pages/[username].js
@@ -20,7 +20,7 @@ export default function ProfilePage({ username }) {
         <title>{capitalized} - ملف شخصي</title>
         <meta property="og:title" content={`${capitalized} - ملف شخصي`} />
         <meta property="og:description" content={`هذه الصفحة تخص ${capitalized}`} />
-        <meta property="og:image" content={`https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi52ZLn3nr1zOwQpETR1TgGydtN2O_ElKvlT8m2sdT9gZOrdIssa8NlFsXPDDTy1YPF9JeAmN68kNpQC4NNw7QFX6XlTJBdLAG0xfyWmHekXPkySGRdMj4et0OxszXdtNke_su3mb5xoaZ0a29aLn3HcdL_8iPFflHw9y0a6miQ84TULXz8H5ELsDI5vNo/s1600/ogimage.jpg`} />
+        <meta content='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi52ZLn3nr1zOwQpETR1TgGydtN2O_ElKvlT8m2sdT9gZOrdIssa8NlFsXPDDTy1YPF9JeAmN68kNpQC4NNw7QFX6XlTJBdLAG0xfyWmHekXPkySGRdMj4et0OxszXdtNke_su3mb5xoaZ0a29aLn3HcdL_8iPFflHw9y0a6miQ84TULXz8H5ELsDI5vNo/s1600/ogimage.jpg' property='og:image'/>
         <meta property="og:url" content={`https://yourdomain.com/${username}`} />
         <meta property="og:type" content="website" />
       </Head>
